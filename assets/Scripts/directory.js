371df39c746b6da0523a7f14783fbd1374b2efa1
Dan Bi <biwuji123@gmail.com>
2020-08-14 14:36:00 -0400

    Added first draft code. current objective is to just get the info on the screen. Not worrying about style

--- assets/Scripts/directory.js
+++ assets/Scripts/directory.js
@@ -1,3 +1,4 @@
+/*-----Reading Data from Database. TODO: Move away from loval file-----*/
 var fr = new FileReader(); // temperory: reading from local file
 
 fr.readAsText("../data/tutors.json")
@@ -6,12 +7,57 @@ var directoryOBJ = JSON.parse(fr.result); // getting tutors.json as an object
 fr.readAsText("../data/tutorList.txt")
 var tutorsList = JSON.parse(fr.result); // getting an array of tutor names
 
+/*-----Filter. TODO: Make filters look prettier-----*/
+var outreachFilter = true;
+var marketingFilter = true;
+var logisticsFilter = true;
+var hrFilter = true;
+var tutorFilter = false;
+var otherFilter = true;
+showAllSelectedCard() // Show page with the default setting above
+
+// change filter's boolean status when clicked, and also update the selected cards
+document.getElementById("outreach").addEventListener("click", () => {outreachFilter = !outreachFilter; showAllSelectedCard();});
+document.getElementById("marketing").addEventListener("click", () => {marketingFilter = !marketingFilter; showAllSelectedCard();});
+document.getElementById("logistics").addEventListener("click", () => {logisticsFilter = !logisticsFilter; showAllSelectedCard();});
+document.getElementById("hr").addEventListener("click", () => {hrFilter = !hrFilter; showAllSelectedCard();});
+document.getElementById("tutors").addEventListener("click", () => {tutorFilter = !tutorFilter; showAllSelectedCard();});
+document.getElementById("others").addEventListener("click", () => {otherFilter = !otherFilter; showAllSelectedCard();});
+
+
+/*-----Display Functions. TODO: Change innerHTML code to fit the design of the website. Add css formatting-----*/
 function showAllSelectedCard() {
 	/*
 	Process: Runs whenever a filter changes
-			 Monitors and updates filter status
+			 
 	Output: calls showCard() on all names selected by the filters
 	*/
+	var id = 0; // ID is within the range {0 - number of names}
+	for (var tutor in tutorsList) {
+		if (isValidName(tutor)) {
+			showCard(tutor, `name_${id}`);
+			id++;
+		}
+	}
+}
+
+function isValidName(name) {
+	/*
+	Input: (STRING) Name of tutor
+	Output: returns a boolean indicating whether or not the name matches the currently selected filters
+	*/
+	for (var position in directoryOBJ[name].position) {
+		if (position === "Director of Outreach" && outreachFilter) {return true;}
+		if (position === "Marketing Team Member" && marketingFilter) {return true;}
+		if ((position === "Logistics Member" || position === "Director of Logistics") && logisticsFilter) {return true;}
+		if (position === "Co-Director of Human Resources" && hrFilter) {return true;}
+		if (position === "Tutor" && tutorFilter) {return true;}
+		if ((position === "Development Team Member" ||
+			position === "Director of Development Team" ||
+			position === "Enrichment Course Instructor" ||
+			position === "Website Assitant" ||) && otherFilter) {return true;}
+	}
+	return false;
 }
 
 function showCard(name, id) {
