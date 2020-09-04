56dbd82564cde442baa52556b0074c3bde2bd9e1
Dan Bi <biwuji123@gmail.com>
2020-09-04 11:41:06 -0400

    added directory and drop down

--- public/assets/Scripts/directory.js
+++ public/assets/Scripts/directory.js
@@ -12,77 +12,19 @@ measurementId: "G-JT2D4NZ9RQ"
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-/*-----Reading Data from Database. TODO: Move away from local file-----*/
+var grid = document.getElementById("grid");
+var allCards = ["DM", "NB", "DB", "DH", "AC", "AR", "AM", "AL", "BS", "BB", "CY", "CB", "GB", "JW", "NR", "SK", "TA"];
 
-var directoryPromise = axios.get('../assets/data/tutors.json')
-  .then(response => {
-  	console.log("Getting tutor data");
-  	return response.data;
-  })
-  .catch(error => {
-	console.error(error);
-	return null;
-  });
-
-var tutorsPromise = axios.get('../assets/data/tutorList.json')
-  .then(response => {
-	console.log("Getting tutorList data");
-	return response.data;
-  })
-  .catch(error => {
-	console.error(error);
-	return null;
-  });
+updatePage("ALL"); // default to All filter
 
 
-/*-----Filter. TODO: Make filters look prettier-----*/
-var outreachFilter = true;
-var marketingFilter = true;
-var hrFilter = true;
-var tutorFilter = false;
-var otherFilter = true;
-showAllSelectedCard() // Show page with the default setting above
-
-
-/*-----Display Functions. TODO: Change innerHTML code to fit the design of the website. Add css formatting-----*/
-function showAllSelectedCard() {
-	/*
-	Process: Runs whenever a filter changes
-			 
-	Output: calls showCard() on all names selected by the filters
-			removes any names not selected by the filters
-	*/
-	directoryPromise.then((directoryOBJ) => {
-		tutorsPromise.then((tutorsList) => {
-			var id = 0; // ID is within the range {0 - number of names}
-			for (var tutorIndex in tutorsList) {
-				if (isValidName(tutorsList[tutorIndex], directoryOBJ)) {
-					showCard(tutorsList[tutorIndex], `name_${tutorIndex}`, directoryOBJ);
-				} else { 
-					document.getElementById(`name_${tutorIndex}`).innerHTML = "";
-				}
-			}
-		});
-	});
-}
-
-// make something like this: https://www.constructdigital.com/about/our-people (once we have all headshots)
-
-// fill each row until we can't (absolute, specify left and top pixels)
-// them move on to the next row
-
-// manually copy and past each box, but just javascript to put in name, img, title, and also position
-
-// Grid fiter --> filter box(flex column) (width changes based on media) --> quote, image, info
-
-// have css classes like "showquote" or "scaleimg" that you can apply with javascript
+new ResizeSensor(document.getElementsByTagName("BODY")[0], function() {
+    updatePage(document.getElementsByClassName("filtr-active")[0].id);
+});
 
 function updatePage(filter) {
 	updateFilters(filter);
-	// TESTING
-	document.getElementById("AC").style.left = "0px";
-	document.getElementById("AC").style.top = "0px";
-	document.getElementById("DM").style.display= "hidden";
+	updateCards(getValidCards(filter));
 }
 
 function updateFilters(filter) {
@@ -101,43 +43,47 @@ function updateFilters(filter) {
 function getValidCards(filter) {
 	// pre: recieves a filter
 	// post: returns a list of IDs of cards that match the filter
-	var cardList = ["DM", "NB", "DB", "DH", "AC", "AL", "BS", "BB", "CB", "CY", "DA", "GB", "JW", "NR", "SK", "TA"];
+	var cardList = [];
+	for (var i in allCards) {
+		if (document.getElementById(allCards[i]).classList.contains(filter)) {
+			cardList.push(allCards[i]);
+		}
+	}
+	return cardList;
 }
 
-function isValidName(name, directoryOBJ) {
-	/*
-	Input: (STRING) Name of tutor, (OBJ) JSON object of Tutors
-	Output: returns a boolean indicating whether or not the name matches the currently selected filters
-	*/
-	var positionsList = directoryOBJ[name].position;
+function updateCards(cardList) {
+	// pre: recieves a list of cards to keep
+	// moves those cards to the front, removes all other cards
+	var cardWidth = document.getElementById(cardList[0]).offsetWidth; // width of each card
+	var cardHeight = document.getElementById(cardList[0]).offsetHeight; // height of each card
 
-	for (var i in positionsList) {
-		if (positionsList[i] === "Director of Outreach" && outreachFilter) {return true;}
-		if ((positionsList[i] === "Director of Social Media" || positionsList[i] === "Marketing Team Member") && marketingFilter) {return true;}
-		if (positionsList[i] === "Co-Director of Human Resources" && hrFilter) {return true;}
-		if (positionsList[i] === "Tutor" && tutorFilter) {return true;}
-		if ((positionsList[i] === "Development Team Member" ||
-			positionsList[i] === "Director of Development Team" ||
-			positionsList[i] === "Enrichment Course Instructor" ||
-			positionsList[i] === "Website Assitant" ||
-			positionsList[i] === "Director of Logistics") && otherFilter) {return true;}
+	var columns = Math.floor(grid.offsetWidth / cardWidth);
+	var rows = Math.floor(cardList.length / columns) + 1;
+	var margin = (grid.offsetWidth - (columns * cardWidth)) / (columns - 1);
+	grid.style.height = (rows * cardHeight) + "px";
+	var left = 0;
+	var top = 0;
+	for (var i in allCards) {
+		if (!(allCards[i] in cardList)) {
+			document.getElementById(allCards[i]).style.opacity = "0";
+		}
 	}
-	return false;
-}
+	for (var i in cardList) {
+		left += cardWidth + margin;
+		if (i % columns == 0) {
+			top += cardHeight;
+			left = 0;
+		}
+		if (i == 0) {
+			top = 0;
+			left = 0;
+		}
 
-function showCard(name, id, directoryOBJ) {
-	/*
-	Input: (STRING) Name of tutor, (STRING) id of HTML element, (OBJ) JSON object of Tutors
-	Output: updates the inner html of the specified HTML element to show tutor info
-			Shows Tutor Name and their Position
-			Example:
-			Dan Bi: Vice President, Tutor
-	*/
-	var positionsList = directoryOBJ[name].position;
-	var positionsString = positionsList[0]; // default string is the first position
 
-	for (var i = 1; i < positionsList.length; i++) { // if there are more positions, loop to get it
-		positionsString += ", " + positionsList[i];
+		document.getElementById(cardList[i]).style.position = "absolute";
+		document.getElementById(cardList[i]).style.opacity = "100%";
+		document.getElementById(cardList[i]).style.left = left + "px";
+		document.getElementById(cardList[i]).style.top = top + "px";
 	}
-	document.getElementById(id).innerHTML = directoryOBJ[name].first + " " + directoryOBJ[name].last + ": " + positionsString;
 }
