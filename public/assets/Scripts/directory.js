931c0f3ab07912796534a9834697c8bb9fe6565b
Dan Bi <biwuji123@gmail.com>
2021-03-25 23:45:25 -0400

    updated team page

--- public/assets/Scripts/directory.js
+++ public/assets/Scripts/directory.js
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 
 var grid = document.getElementById("grid");
 
-var allCards = ["DM", "NB", "DB", "AC", "AL", "BS", "BB", "CBR", "CY", "DH", "ET", "GB", "CB", "JH", "JT", "JW", "LL", "NR", "SK", "TA", "AR", "AM", "FA"];
+var allCards = ["DM", "NB", "DB", "GB", "AC", "AL", "BS", "BB", "CBR", "CY", "DH", "ET", "CB", "JH", "JT", "JW", "LL", "NK", "NR", "PV", "SK", "TA", "AR", "AM", "FA"];
 //If initials repeat, simply include the second letter of their last name (and so on).
 
 updatePage("ALL"); // default to All filter
